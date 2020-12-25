b840406c5eb2e5c880b54f0185aa91776fcf2db9
Joseph Baranauskas <joseph.baranauskas@live.com>
2020-12-25 13:42:28 -0700

    Adding Libraries project to solution including MongoDB package.

--- ClassicASPWebsite/Repositories/Page-Resources/JS/version-switch.js
+++ ClassicASPWebsite/Repositories/Page-Resources/JS/version-switch.js
@@ -1,3 +1,19 @@
-﻿function setVersion() {
+﻿const objIdEmpty = 'ObjectId("")';
+const objIdTenant = 'ObjectId("57b2229fd00bc11c2c18ec3d")';
+
+$(document).ready(function () {
+    $("#span-tenantId").html(objIdEmpty);
+});
+
+function setVersion() {
     alert("Set version to: ");
+    
+}
+
+function migrateVersionTo() {
+    alert("migrateVersionTo > ");
+}
+
+function migrateVersionFrom() {
+    alert("migrateVersionFrom < ");
 }
